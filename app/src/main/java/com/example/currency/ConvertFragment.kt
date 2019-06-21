24d2f9c24d77d858e34ed2ad442e30ac2ea0ec06
MaricaMithens <mari.aleo@yandex.ru>
2019-06-21 17:49:18 +0300

    all screens is ready

--- app/src/main/java/com/example/currency/ConvertFragment.kt
+++ app/src/main/java/com/example/currency/ConvertFragment.kt
@@ -6,11 +6,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.fragment_convert.*
 import kotlinx.android.synthetic.main.fragment_convert.changeDateButton
 import kotlinx.android.synthetic.main.fragment_convert.tvDate
-import kotlinx.android.synthetic.main.fragment_curr.*
-import java.time.Year
 import java.util.*
 import javax.xml.datatype.DatatypeConstants
 
@@ -19,6 +18,7 @@ class ConvertFragment: Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
     }*/
+    var curr_values: MutableMap<String, Double> = mutableMapOf("RUB" to 1.0, "USD" to 6.0, "EUR" to 8.1, "JPY" to 9.3)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //return super.onCreateView(inflater, container, savedInstanceState)
@@ -28,6 +28,14 @@ class ConvertFragment: Fragment() {
 
     override fun onStart() {
         super.onStart()
+        arg2.keyListener = null
+        val currList = arrayOf("RUB", "USD", "EUR", "JPY")
+        val adapter = ArrayAdapter(this.context, android.R.layout.simple_spinner_item, currList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        val adapter2 = ArrayAdapter(this.context, android.R.layout.simple_spinner_item, currList)
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = adapter2
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
@@ -37,10 +45,36 @@ class ConvertFragment: Fragment() {
         changeDateButton.setOnClickListener {
             chooseDate(year, month, day)
         }
+        button.setOnClickListener {
+            convert()
+        }
+    }
+
+    private fun convert(){
+        //вызвать запрос??
+        val curr1 = spinner.getItemAtPosition(spinner.selectedItemPosition)
+        val curr2 = spinner2.selectedItem
+        var val1 = "0.00"
+        if (arg1.text.isEmpty()) {arg1.setText(val1)}
+        val1 = arg1.text.toString()
+        val date = tvDate.text.toString().replace(".", "/")
+        Log.d("HAHAH", "curr 1 $curr1   curr2 $curr2  date $date  val1 $val1")
+        if (val1 == "0.00")  {
+            Log.d("HAHAH", "curr 1 $curr1   curr2 $curr2  date $date  val1 $val1")
+            arg2.setText(val1)
+            return
+        }
+       // var va = Math.round(val1.toDouble() * 40 * 100.0) / 100.0
+        var val2 = converter(curr1.toString(), curr2.toString(), val1.toDouble())
+        val res = val2?.let { Math.round(it * 100.0) / 100.0 }
+        arg2.setText("$res")
 
     }
 
-
+    private fun converter(curr1:String, curr2:String, val3:Double): Double? {
+        var val2 = (curr_values[curr1])?.times(val3)?.div(curr_values[curr2]!!)
+        return val2
+    }
 
     private fun chooseDate(current_year : Int, current_month: Int, current_day: Int){
         val dpd = DatePickerDialog(this.context, DatePickerDialog.OnDateSetListener { _, year1, monthOfYear, dayOfMonth ->
@@ -51,6 +85,7 @@ class ConvertFragment: Fragment() {
         dpd.datePicker.minDate = 0
         dpd.show()
     }
+
 }
 
 private operator fun DatatypeConstants.Field.get(monthOfYear: Int): Any? {
