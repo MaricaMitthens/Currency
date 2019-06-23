732a4cd1853bcc91331ed11c08744f8c9ca36d71
Mary <mari.aleo@yandex.ru>
2019-06-23 16:44:08 +0300

    all functions implemented

--- app/src/main/java/com/example/currency/CurrencyRateOperation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.currency
-
-import com.google.gson.annotations.SerializedName
-
-
-data class CurrencyRateOperation(
-    @SerializedName("operationDate") val operationDate: String,
-    @SerializedName("rateDate") val rateDate: String
-) : HistoryOperation(operationDate)
